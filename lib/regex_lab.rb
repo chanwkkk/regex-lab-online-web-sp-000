96d20854be51db3b38672b6071f2f8062b8e117e
Yingqi Chen <chanwk18@hotmail.com>
2019-03-21 07:32:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,9 +21,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-if phone.scan(/\b\d{3}.*\d{3}.*\d{4}\b/)
-  true
-else
-  false
+!!phone.scan(/\b\d{3}.*\d{3}.*\d{4}\b/)
+
 end
 end
