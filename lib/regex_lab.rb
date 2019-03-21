137ca124ef4349689ddb3ab2be2ecb80a40fbcdd
Yingqi Chen <chanwk18@hotmail.com>
2019-03-21 06:21:57 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,24 @@
 def starts_with_a_vowel?(word)
+  !!word.match(/^[aeiouAEIOU]\w+/)
+  #  true
+  #else false
+
+#end
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  if text.match(/^un\w+ing$/)
 
+
+  end
 end
 
 def words_five_letters_long(text)
-
+  if text.scan(/\w{5}\s/)
+    true
+  else false
+  end
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
